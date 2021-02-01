180d107be7dc88e07afa97c556b05168915e5b6e
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-02-01 07:31:25 +0100

    is_polydivisible tests pass

--- polydivisible_numbers/lib/polydivisible_numbers.ex
+++ polydivisible_numbers/lib/polydivisible_numbers.ex
@@ -3,11 +3,21 @@ defmodule PolydivisibleNumbers do
   Documentation for `PolydivisibleNumbers`.
   """
 
-  def is_polydivisible(s, b) do
-    # your code here
-  end
-
   @chars "0123456789"
+
+  def is_polydivisible(s, b) do
+    s
+    |> String.codepoints()
+    |> Enum.with_index(1)
+    |> Enum.reduce_while("", fn {number_char, divisor}, acc ->
+      number_chars = acc <> number_char
+      number = to_base10(number_chars, b)
+
+      if rem(number, divisor) == 0,
+        do: {:cont, number_chars},
+        else: {:halt, false}
+    end) != false
+  end
 
   # def get_polydivisible(n, b) do
   #   # your code here
@@ -20,7 +30,7 @@ defmodule PolydivisibleNumbers do
     |> Enum.with_index(0)
     |> Enum.reduce(0, fn {number_char, exponent}, acc ->
       number = index_of(number_char, @chars)
-      number * :math.pow(b, exponent) + acc
+      number * trunc(:math.pow(b, exponent)) + acc
     end)
   end
 
